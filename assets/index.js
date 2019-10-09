77c6ab88847d7a9bc9f2fff11622636d504b4e6e
Juan_Prieto <gadaf.78@hotmail.com>
2019-10-08 22:19:29 -0500

    el primer plugin
    *Se crea el autoplay.js
    * mediaplayer.js: se crea this.plugins y se asignan los prototipos de mute e init plugins
    *index.js: se instancia autoplay a media player

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,17 @@
 import mediaPlayer from './mediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video"); //query selector representa un selector o varios
 const button = document.querySelector("button");
 
-const player = new mediaPlayer({elemento: video})
+const player = new mediaPlayer({elemento: video, plugins: [new AutoPlay()]})
 
 
 button.addEventListener('click', function(){
    video.paused ? player.play() : player.pause();
+})
+
+document.getElementById('muteButton').addEventListener('click', function(){
+   player.toggleMute();
 })
 
--- assets/mediaPlayer.js
+++ assets/mediaPlayer.js
@@ -1,5 +1,8 @@
 function mediaPlayer(config){
-    this.media = config.elemento
+    this.media = config.elemento;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
 }
 
 mediaPlayer.prototype.play = function(){
@@ -8,7 +11,24 @@ mediaPlayer.prototype.play = function(){
 
 mediaPlayer.prototype.pause = function(){
     this.media.pause();
-    
+}
+
+mediaPlayer.prototype.mute = function(){
+    this.media.muted = true;
+}
+
+mediaPlayer.prototype.unmute = function(){
+    this.media.muted = false;
+}
+
+mediaPlayer.prototype._initPlugins = function(){
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    })
+}
+
+mediaPlayer.prototype.toggleMute = function(){
+    this.media.muted ? this.media.muted = false : this.media.muted = true;
 }
 
 export default mediaPlayer
