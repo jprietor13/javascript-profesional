a9f45ae81b4bbc92714f9529f5dd41b4c31aab7f
Juan_Prieto <gadaf.78@hotmail.com>
2019-10-12 13:58:07 -0500

    this:
    Se describe el uso de this el los diferentes scopes de javascript (objetos, funciones, clases)

--- assets/index.js
+++ assets/index.js
@@ -4,7 +4,10 @@ import AutoPlay from './plugins/AutoPlay.js';
 const video = document.querySelector("video"); //query selector representa un selector o varios
 const button = document.querySelector("button");
 
-const player = new mediaPlayer({elemento: video, plugins: [new AutoPlay()]})
+const player = new mediaPlayer({elemento: video, plugins: [
+   //new AutoPlay()
+   ]
+});
 
 
 button.addEventListener('click', function(){
