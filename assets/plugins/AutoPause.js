699028d9487f8cb765966ff34b96fa17f47a4b33
Juan_Prieto <gadaf.78@hotmail.com>
2019-10-17 19:16:42 -0500

    visibilityChange
    Se describe el uso del visibilityChange con el video

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
     constructor(){
         this.umbral = 0.25;
         this.handlerIntersection = this.handlerIntersection.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
     }
     run(player){
         this.player = player;
@@ -10,7 +11,9 @@ class AutoPause {
         });
 
         //el observer observara el elemento media y el contenedor es toda la pantalla
-        observer.observe(this.player.media)
+        observer.observe(this.player.media);
+
+        document.addEventListener("visibilitychange", this.handleVisibilityChange)
         
     }
 
@@ -23,6 +26,11 @@ class AutoPause {
         } else {
             this.player.pause();
         }
+    }
+
+    handleVisibilityChange(){
+        const isVisible = document.visibilityState === "visible";
+        isVisible ? this.player.play() : this.player.pause();
     }
 }
 
